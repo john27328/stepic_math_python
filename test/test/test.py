2d42037f3b3ae1e6374bf5b4566ef69fb28890f8
Ivan Raptev <john27328@gmail.com>
2019-11-17 15:00:32 +0300

    1.4

--- test/test/test.py
+++ test/test/test.py
@@ -1,5 +1,13 @@
-# put your python code here
+#your code
+a = 2
+b = 3
+def sum2(a, b):
+    if(type(a) in (int, float) and type(b) in (int, float)): 
+        return a + b
+    if(type(b) in (int, float)): 
+        return "1st argument is not a number"
+    if(type(a) in (int, float)): 
+        return "2nd argument is not a number"
+    return "all arguments are not a numbers"
 
-i1 = int(input())
-i2 = int(input())
-print(i1+i2)
+print(sum2(a,b))
